475ffff0a19cdbff59c820965f6439cb6ebf49ab
warleyhenrique <warley.henrique93@gmail.com>
2021-08-29 17:28:31 -0300

    Fix: add todos

--- src/index.js
+++ src/index.js
@@ -70,7 +70,24 @@ app.post('/todos', checksExistsUserAccount, (request, response) => {
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const {title, deadline} = request.body;
+  const { id } = request.params;
+  const { user } = request;
+
+  const todo = user.todos.filter(
+    (todo)=>
+    todo.id === id
+  );
+
+  if(!todo){
+    return response.status(404).json({error: "todo not found"});
+  }
+  
+  todo.title = title;
+  todo.deadline = new Date(deadline);
+
+  return response.json(todo);
+
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
